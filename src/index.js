eaf77798ccb03930ea0de06e1714e0296d6f5030
pi-da <jpcox@byui.edu>
2019-11-07 11:09:45 -0700

    added firebase config

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyA6zBVnlAtngdmKBHPColrHSCAgwhm_HbI",
+    authDomain: "raspberry-pi-data-analytics.firebaseapp.com",
+    databaseURL: "https://raspberry-pi-data-analytics.firebaseio.com",
+    projectId: "raspberry-pi-data-analytics",
+    storageBucket: "raspberry-pi-data-analytics.appspot.com",
+    messagingSenderId: "161315917592",
+    appId: "1:161315917592:web:00cc58c6c9d6580f6a186d",
+    measurementId: "G-14VSFT2TXY"
+  };
+
+  firebase.initalizeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
