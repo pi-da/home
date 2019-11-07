93755d4a1693bbbabb7ec31afcf75cd064024b80
pi-da <jpcox@byui.edu>
2019-11-06 22:29:29 -0700

    Updates

---
+++ precache-manifest.43c97f163c03c74adfcff4172b0a3d5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c3c34093322b6458b94579d839d5010",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f583a8edf89e51c5654",
+    "url": "./static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9ecad117e7608ced293f",
+    "url": "./static/js/2.3385f2ef.chunk.js"
+  },
+  {
+    "revision": "7f583a8edf89e51c5654",
+    "url": "./static/js/main.915c7273.chunk.js"
+  },
+  {
+    "revision": "4f6e21c78bc573c153c9",
+    "url": "./static/js/runtime-main.505c8e1f.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
